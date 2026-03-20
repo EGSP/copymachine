b06d6b72fe4385fe50554c17a72207c4fed8c5e6
tatapstar <repsbro@gmail.com>
2026-03-20 21:45:37 +0300

    server\fix: Функция старта сервера переделана в правильную серверную функци TanStack

--- src/actions/background.functions.ts
+++ src/actions/background.functions.ts
@@ -1,13 +1,16 @@
 
-import { getServer, BackgroundServer } from '../background/background.server'
+import { createServerFn } from "@tanstack/react-start";
+import { BackgroundServer, getServer } from "../background/background.server";
 
-export async function ensureBackgroundServer() {
-    let server = getServer()
-    if (!server) {
-        server = await new BackgroundServer().ini()
-    }
-    if (server) {
-        return "Background server is running"
-    }
-    throw new Error("Failed to start background server")
-}
+export const ensureBackgroundServer = createServerFn({ method: "POST" }).handler(
+	async () => {
+		let server = getServer();
+		if (!server) {
+			server = await new BackgroundServer().ini();
+		}
+		if (server) {
+			return "Background server is running";
+		}
+		throw new Error("Failed to start background server");
+	},
+);
